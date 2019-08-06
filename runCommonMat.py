4e6974a6a0cbf53cbd7dd7bfc02c84e6b11d670d
Vicki Khalfin <khalfinv@gmail.com>
2019-08-06 16:24:59 +0300

    added correlation analyzing and brain plotting

--- runCommonMat.py
+++ runCommonMat.py
@@ -34,7 +34,11 @@ import matplotlib.gridspec as gridspec
 import time
 import networkToIndexDic
 import itertools
+import matplotlib.patches as mpatches
 
+
+labelToColorDic = {"Uncertain" : "olive", "SSH" : "cyan", "SSM" : "orange", "CO" : "purple", "Auditory" : "m", "DMN" : "red", "Memory" : "grey", 
+	"Visual" : "blue", "FP" : "gold", "Salience" : "black", "Subcortical" : "brown", "VAN" : "teal", "DAN" : "green", "Cerebellum" : "purple"}
 
 def createCommonMat(subjects_data_dict, exclusion_criteria):
     """Create common covariance and correlation matrices
@@ -74,8 +78,6 @@ def plotMatrix(matrix, plot_path, labels, title, ticks, vmin, vmax):
 	return: None
     """
     ticks = list(map(lambda x: x - 0.5, ticks))
-    labelToColorDic = {"Uncertain" : "olive", "SSH" : "cyan", "SSM" : "orange", "CO" : "purple", "Auditory" : "m", "DMN" : "red", "Memory" : "grey", 
-	"Visual" : "blue", "FP" : "gold", "Salience" : "black", "Subcortical" : "brown", "VAN" : "teal", "DAN" : "green", "Cerebellum" : "purple"}
     ticks_middle = [(((ticks[i+1]-ticks[i]) / 2 ) + ticks[i]) for i in range(0,len(ticks)-1)]
     fig, ax = plt.subplots()
     fig.set_size_inches(16.5, 9.5)
@@ -91,12 +93,49 @@ def plotMatrix(matrix, plot_path, labels, title, ticks, vmin, vmax):
     plotting.plot_matrix(matrix, colorbar=True, figure=fig, vmin=vmin, vmax=vmax)
     fig.savefig(plot_path)
     plt.close()
+
+def analyzeCorrelationsAndPlotConnectome(matrix, coords, networks, out_folder):
+	colors = []
+	patches_list = []
+	index2network = {}
+	min_r = 0.7
+	last_index = 0
+	for network in networks:
+		network_color = [labelToColorDic[network]] * len(list(networkToIndexDic.dic[network]))
+		colors = colors + network_color
+		for i in range(last_index, last_index + len(list(networkToIndexDic.dic[network]))):
+			index2network[i] = network
+			last_index = i+1
+		network_patch = mpatches.Patch(color=labelToColorDic[network], label=network)
+		patches_list.append(network_patch)
+	correlated_coords = {} 
+	for i in range(1,len(matrix)):
+		for j in range(i):
+			if(matrix[i][j] >= min_r):
+				correlated_coords[matrix[i][j]] = ({index2network[i] : coords[i]}, {index2network[j] : coords[j]})
+	correlated_coords_file = open(out_folder + "/correlated_coords_" + str(networks) + ".txt", "w")
+	for item in correlated_coords.items():
+		correlated_coords_file.write(str(item)+ "\n")
+	correlated_coords_file.close()
+	plot_name = out_folder + "/brain_plot" + str(networks) + ".png"
+	if(len(correlated_coords) > 0):
+		plotConnectome(matrix, coords, plot_name, colors, patches_list, min_r)
 	
-def runAllCombinations(common_cov_mat, common_cor_mat, out_folder):
+def plotConnectome(matrix, coords, plot_path, colors, patches_list, min_r):
+	fig = plt.figure()
+	title = "Threshold : " + str(min_r)
+	plotting.plot_connectome(adjacency_matrix= matrix, node_coords= coords, title = title, node_color = colors, colorbar=True, edge_threshold=min_r ,
+		node_size = 5, edge_vmin = -1, edge_vmax = 1, figure=fig, display_mode  = 'lyrz')
+	fig.legend(handles=patches_list, loc = "upper center")
+	fig.savefig(plot_path)
+	plt.close()
+	
+def runAllCombinations(common_cov_mat, common_cor_mat, out_folder, coords):
     """Create plots of ovariance and correlation matrices for all combinations of 2 networks. 
 	param common_cov_mat: two dimensional array (264,264). The common covariance matrix with all networks
-	param common_cor_mat : two dimensional array (264, 264).The common correlation matrix with all networks
+	param common_cor_mat: two dimensional array (264, 264).The common correlation matrix with all networks
     param out_folder: string. The path of output folder
+	param coords: list.List of atlas coordinates
 	return: None
     """
     for pair in itertools.combinations(networkToIndexDic.dic.keys(), r=2):
@@ -109,13 +148,16 @@ def runAllCombinations(common_cov_mat, common_cor_mat, out_folder):
         listToSlice = listToSlice + list(networkToIndexDic.dic[network1])
         listToSlice = listToSlice + list(networkToIndexDic.dic[network2])
         common_cov_mat_sliced = common_cov_mat[listToSlice, :][:, listToSlice] 
-        common_cor_mat_sliced = common_cor_mat[listToSlice, :][:, listToSlice] 
+        common_cor_mat_sliced = common_cor_mat[listToSlice, :][:, listToSlice]		
+        coords_sliced = [coords[i] for i in listToSlice]
         ticks = [0, len(networkToIndexDic.dic[network1]), len(networkToIndexDic.dic[network1]) + len(networkToIndexDic.dic[network2])]                                                                                                                                                                                                                                              
         plotMatrix(common_cor_mat_sliced, out_folder + "/common_cor_matrix_" + network1 + "_" + network2 + ".png", [network1, network2], "Common correlation matrix", ticks, -1., 1.)
         plotMatrix(common_cov_mat_sliced, out_folder + "/common_cov_matrix_" + network1 + "_" + network2 + ".png", [network1, network2], "Common covariance matrix",
                    ticks, common_cov_mat_sliced.min(), common_cov_mat_sliced.max())
+        analyzeCorrelationsAndPlotConnectome(common_cor_mat_sliced, coords_sliced, pair, out_folder)
+
 				   
-def runSomeNetworks(common_cov_mat,common_cor_mat,out_folder, networks ):
+def runSomeNetworks(common_cov_mat,common_cor_mat,out_folder, networks , coords):
     """Create plots of covariance and correlation matrices for specific networks 
 	param common_cov_mat: two dimensional array (264,264). The common covariance matrix with all networks
 	param common_cor_mat : two dimensional array (264, 264).The common correlation matrix with all networks
@@ -136,54 +178,74 @@ def runSomeNetworks(common_cov_mat,common_cor_mat,out_folder, networks ):
             print ( "The " + network + " network does not exist!!!")
     common_cov_mat_sliced = common_cov_mat[listToSlice, :][:, listToSlice] 
     common_cor_mat_sliced = common_cor_mat[listToSlice, :][:, listToSlice] 
+    coords_sliced = [coords[i] for i in listToSlice]
     plotMatrix(common_cor_mat_sliced, out_folder + "/common_cor_matrix_" + str(networks) + ".png", networks, "Common correlation matrix", ticks, -1., 1.)
     plotMatrix(common_cov_mat_sliced, out_folder + "/common_cov_matrix_" + str(networks) + ".png", networks, "Common covariance matrix", ticks,
              	common_cov_mat_sliced.min(), common_cov_mat_sliced.max())
+    analyzeCorrelationsAndPlotConnectome(common_cor_mat_sliced, coords_sliced, networks, out_folder)
+
+
 				   
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--out_folder', required=True, type=str, help='output folder for common matrices pkl file (if generated) and plots')
-    parser.add_argument('--exclusion_criteria', required=False, type=int, default=375, help='Number of volumes for subjects exclusion. Default is 375 (5 minutes scan)')
-    parser.add_argument('--use_prev', help='use previous generated covariance and correlation matrices',action='store_true')
-    parser.add_argument('--common_mat_pkl', required=False, type=str, help='path to common matrices pkl file, mandatory only if use_prev flag exists')
-    parser.add_argument('--subjects_data_dict', required=False, type=str, help='path to pkl file containing the subjects data dictionary, mandatory only if use_prev flag does not exist')
-    parser.add_argument('--networks', required=False, nargs='+', help='Plot the common matrices for some networks, if you want the combination of every two networks use --networks all')
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--out_folder', required=True, type=str, help='output folder for common matrices pkl file (if generated) and plots')
+	parser.add_argument('--exclusion_criteria', required=False, type=int, default=375, help='Number of volumes for subjects exclusion. Default is 375 (5 minutes scan)')
+	parser.add_argument('--use_prev', help='use previous generated covariance and correlation matrices',action='store_true')
+	parser.add_argument('--common_mat_pkl', required=False, type=str, help='path to common matrices pkl file, mandatory only if use_prev flag exists')
+	parser.add_argument('--subjects_data_dict', required=False, type=str, help='path to pkl file containing the subjects data dictionary, mandatory only if use_prev flag does not exist')
+	parser.add_argument('--networks', required=False, nargs='+', help='Plot the common matrices for some networks, if you want the combination of every two networks use --networks all')
+	parser.add_argument('--atlas', required=False, help='path to atlas coordinates file. Needed only if --networks flag exists')
+	args = parser.parse_args()
 	#Arguments checks
-    if (args.use_prev == True and args.common_mat_pkl is None):
-        print ("--common_mat_pkl is required. Use -h for more details")
-        exit(1)
-    elif (args.use_prev == False and args.subjects_data_dict is None):
-        print ("--subjects_data_dict is required. Use -h for more details")
-        exit(1)
-	
-    if args.use_prev is False:
-	    # generated new common covariance and correlation matrices
-        print("Read from subjects' data pkl")
-        pkl_file = open(args.subjects_data_dict, 'rb')
-        allParticipantsDict = pickle.load(pkl_file)
-        pkl_file.close()
-        (common_cov_mat, common_cor_mat) = createCommonMat(allParticipantsDict, args.exclusion_criteria)
-        print("Write common matrices to pkl file")
-        f = open(args.out_folder + '/commonCovAndCorMat.pkl', mode="wb")
-        pickle.dump((common_cov_mat, common_cor_mat), f)
-        f.close() 
-    else:
-        #use previous version of common matrices
-        print("Read from common matrices pkl file")
-        pkl_file = open(args.common_mat_pkl, 'rb')
-        (common_cov_mat, common_cor_mat) = pickle.load(pkl_file)
-        pkl_file.close()
-	
-    if (args.networks != None):
-        if(args.networks == ["all"]):
-            runAllCombinations(common_cov_mat, common_cor_mat, args.out_folder) 
-        else:
-            runSomeNetworks(common_cov_mat, common_cor_mat,args.out_folder,args.networks)
+	if (args.use_prev == True and args.common_mat_pkl is None):
+		print ("--common_mat_pkl is required. Use -h for more details")
+		exit(1)
+	elif (args.use_prev == False and args.subjects_data_dict is None):
+		print ("--subjects_data_dict is required. Use -h for more details")
+		exit(1)
+	if (args.networks != None and args.atlas is None):
+		print ("--atlas is required. Use -h for more details")
+		exit(1)
+
+	if args.use_prev is False:
+		# generated new common covariance and correlation matrices
+		print("Read from subjects' data pkl")
+		pkl_file = open(args.subjects_data_dict, 'rb')
+		allParticipantsDict = pickle.load(pkl_file)
+		pkl_file.close()
+		(common_cov_mat, common_cor_mat) = createCommonMat(allParticipantsDict, args.exclusion_criteria)
+		print("Write common matrices to pkl file")
+		f = open(args.out_folder + '/commonCovAndCorMat.pkl', mode="wb")
+		pickle.dump((common_cov_mat, common_cor_mat), f)
+		f.close() 
+	else:
+		#use previous version of common matrices
+		print("Read from common matrices pkl file")
+		pkl_file = open(args.common_mat_pkl, 'rb')
+		(common_cov_mat, common_cor_mat) = pickle.load(pkl_file)
+		pkl_file.close()
+
+	#extract coordinates from power atlas
+	mniCoordsFile = open(args.atlas,"rb")
+	coords = []
+	for line in mniCoordsFile.read().splitlines():
+		splitedLine = line.decode().split(' ')
+		newCoord = []
+		for part in splitedLine:
+			if part is not '':
+				newCoord.append(float(part))
+		coords.append(newCoord)
+	mniCoordsFile.close()
+
+	if (args.networks != None):
+		if(args.networks == ["all"]):
+			runAllCombinations(common_cov_mat, common_cor_mat, args.out_folder, coords) 
+		else:
+			runSomeNetworks(common_cov_mat, common_cor_mat,args.out_folder,args.networks, coords)
 
 	#Plot the full common matrices
-    ticks = [0,28,58,63,77,90,148,153,184,209,227,240,249,260,264]
-    plotMatrix(common_cor_mat, args.out_folder + "/common_cor_matrix.png",networkToIndexDic.dic.keys(), "Common correlation matrix", ticks, -1., 1.)
-    plotMatrix(common_cov_mat, args.out_folder + "/common_cov_matrix.png",networkToIndexDic.dic.keys(), "Common covariance matrix",
-                   ticks, common_cov_mat.min(), common_cov_mat.max())
+	ticks = [0,28,58,63,77,90,148,153,184,209,227,240,249,260,264]
+	plotMatrix(common_cor_mat, args.out_folder + "/common_cor_matrix.png",networkToIndexDic.dic.keys(), "Common correlation matrix", ticks, -1., 1.)
+	plotMatrix(common_cov_mat, args.out_folder + "/common_cov_matrix.png",networkToIndexDic.dic.keys(), "Common covariance matrix",
+				   ticks, common_cov_mat.min(), common_cov_mat.max())
