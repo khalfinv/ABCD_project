9518d3e05bc49fdeffea1deb5ac4acc5174e982f
Vicki Khalfin <khalfinv@gmail.com>
2019-08-21 16:12:44 +0300

    added comments

--- Deep_Learning/split_to_datasets.py
+++ Deep_Learning/split_to_datasets.py
@@ -1,26 +1,54 @@
+#!/usr/bin/python3
+"""
+==========================================================================================
+Split ABCD subjects to 3 datasets (time-series (matrix: 264*375), behavioural score (float))
+train, validation and test
+==========================================================================================
+
+@Input:  
+dicts_pkl = string. path to pkl file with test-validate-train dictionaries (the post processing dictionary splitted to 3)
+score_file = string. path to excel file containing the behavioural scores
+score_key = string. name of the test score column
+out_folder = string. path to output folder. Default is current folder. 
+
+@Output:
+train_set.pkl file: train dataset
+validate_set.pkl file: validate dataset 
+test_set.pkl file: test dataset 	
+
+"""
+
+
 import os, sys, pickle, argparse, re
 import pandas as pd
 
-def createTimeSeriesSets(dict, score_file, score_key):
+def createDataset(dict, score_file, score_key):
+	"""Create dataset - time-series (matrix: 264*375), behavioural score (float)
+	param dict: dictionary. Subject's data dictionary from post processing step
+	param scores_file: DataFrame . File with subject's behavioural scores and data
+	param score_key : string. The wanted score's column name in scores_file
+	return: (time_series, scores) : tuple. 
+		time_series - list of time-series matrices of size (264, 375), scores - list of behavioural scores
+    """
 	time_series = []
 	scores = []
-	for key,value in dict.items():
-		time_series.append((value["time_series"][:375]).T)
+	for key,value in dict.items():	
+		time_series.append((value["time_series"][:375]).T) 	# Get the first 375 frames
 		raw = score_file.loc[lambda df: score_file['SUBJECTKEY'] == key] #Get the raw from the excel that match the subject_key. The raw is from type pandas.series
 		score = raw[score_key].values[0]
 		scores.append(score)
-	return time_series, scores
+	return (time_series, scores)
 	
 	
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--dicts_pkl', required=True, type=str, help='path to pkl file with test-validat-train dictionaries')
+	parser.add_argument('--dicts_pkl', required=True, type=str, help='path to pkl file with test-validate-train dictionaries')
 	parser.add_argument('--score_file', required=True, type=str, help='path to excel file containing the scores')
 	parser.add_argument('--score_key', required=True, type=str, help='name of the test score column')
 	parser.add_argument('--out_folder', required=False, type=str, default=".", help='path to output folder. Default is current folder')
 	args = parser.parse_args()
 
-	#open and read the pkl file of all subjects' matrices	
+	#open and read the pkl file of train-validate-test dictionaries
 	pkl_file = open(args.dicts_pkl, 'rb')
 	all_dicts = pickle.load(pkl_file)
 	pkl_file.close()
@@ -44,11 +72,12 @@ if __name__ == "__main__":
 	for i, row in df.iterrows(): 
 		df.at[i,'SUBJECTKEY'] = re.sub(r"[^a-zA-Z0-9]","",df.at[i,'SUBJECTKEY'])
 
-	train_set = createTimeSeriesSets(train_dict, df, args.score_key)
-	validate_set = createTimeSeriesSets(validate_dict, df, args.score_key)
-	test_set = createTimeSeriesSets(test_dict, df, args.score_key)
+	#create datasets
+	train_set = createDataset(train_dict, df, args.score_key)
+	validate_set = createDataset(validate_dict, df, args.score_key)
+	test_set = createDataset(test_dict, df, args.score_key)
 	
-	print("Save to pkl")
+	#Save the datasets
 	train_file = open(args.out_folder + '/train_set.pkl', mode="wb")
 	pickle.dump(train_set, train_file)
 	train_file.close() 
--- Deep_Learning/split_to_dicts.py
+++ Deep_Learning/split_to_dicts.py
@@ -1,7 +1,36 @@
+#!/usr/bin/python3
+"""
+==========================================================================================
+Split ABCD subjects (after post-processing ) to 3 dictionaries with the following percentage:
+60% train, 20% validation and 20% test
+==========================================================================================
+
+@Input:  
+exclusion_criteria = integer. Number of volumes for subjects exclusion. Default is 375 (5 minutes scan)
+subjects_data_dict = string. path to pkl file containing the subjects data dictionary after post processing
+out_folder = string. path to output folder. Default is current folder. 
+
+@Output:
+split_dicts.pkl file: a dictionary with 3 dictionaries with the following format
+					all_dicts = {
+						'train_dict': train_dict,
+						'validate_dict': validate_dict,
+						'test_dict': test_dict
+					}
+
+"""
+
 import os, sys, pickle, argparse
 import math
 
 def splitToDicts(subjects_dict):
+	"""Split ABCD subjects (after post-processing ) to 3 dictionaries with the following percentage:
+		60% train, 20% validation and 20% test
+	param subjects_dict: dictionary. Subject's data dictionary from post processing step
+	return: train_dict: dictionary. Subjects for training
+			validate_dict: dictionary. Subjects for validation
+			test_dict: dictionary. Subjects for testing
+    """
 	num_of_train = math.floor(len(subjects_dict)*0.6)
 	num_of_validate = math.floor(len(subjects_dict)*0.2)
 	num_of_test = len(subjects_dict) - (num_of_train + num_of_validate)
@@ -10,7 +39,7 @@ def splitToDicts(subjects_dict):
 	validate_dict = {k:subjects_dict[k] for k in list(subjects_dict)[num_of_train : (num_of_train + num_of_validate)]}
 	test_dict = {k:subjects_dict[k] for k in list(subjects_dict)[-num_of_test:]}
 	print("num_of_train: ", num_of_train, " num_of_validate: " , num_of_validate, " num_of_test: ", num_of_test)
-	return train_dict,validate_dict,test_dict  
+	return train_dict, validate_dict, test_dict  
 	
 	
 	
@@ -21,18 +50,19 @@ if __name__ == "__main__":
 	parser.add_argument('--out_folder', required=False, type=str, default=".", help='path to output folder. Default is current folder')
 	args = parser.parse_args()
 
-	#open and read the pkl file of all subjects' matrices	
+	#open and read the pkl file of all subjects' data	
 	pkl_file = open(args.subjects_data_dict, 'rb')
 	subjects_data_dict = pickle.load(pkl_file)
 	pkl_file.close()
 	
 	
-	#Create dictionary with included subject - num_of_volumes >= exclusion_criteria
+	#Create dictionary with only included subject - num_of_volumes >= exclusion_criteria
 	only_included_subjects = {}
 	for key, val in subjects_data_dict.items():
 		if val["num_of_volumes"] >= args.exclusion_criteria:
 			only_included_subjects[key] = val
 	
+	#split the subjects to 3 datasets
 	train_dict, validate_dict, test_dict = splitToDicts(only_included_subjects)
 		
 	all_dicts = {
@@ -41,7 +71,7 @@ if __name__ == "__main__":
 	'test_dict': test_dict
 	}
 
-	print("Save to pkl")
+	#save to pkl
 	f = open(args.out_folder + '/split_dicts.pkl', mode="wb")
 	pickle.dump(all_dicts, f)
 	f.close() 
