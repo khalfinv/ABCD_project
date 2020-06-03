fd32a4cfd3d01130eefa16236b2fcf288810ef0a
Vicki Khalfin <khalfinv@gmail.com>
2020-06-03 12:02:10 +0300

    list of gordon networks by order and script for creating csv data file with only cognitive networks

---
+++ gordon_correlations_algorithms/Gordon_networks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+==========================================================================================================================
+Dictionary of all power networks. Key: network's name, Value: the indexes according to the order of the Power coordinates.
+The indexes are suitable for the common correlation and covariance matrices.
+==========================================================================================================================
+"""
+
+import numpy as np
+
+networks_list=["Auditory-Auditory", "Auditory-CO", "Auditory-CP", "Auditory-DMN", "Auditory-DAN", "Auditory-FP", "Auditory-none", "Auditory-retrosplenial temporal",
+"Auditory-SSH", "Auditory-SSM", "Auditory-Salience", "Auditory-VAN", "Auditory-Visual", "CO-Auditory", "CO-CO", "CO-CP", "CO-DMN", "CO-DAN", "CO-FP",
+"CO-none", "CO-retrosplenial temporal", "CO-SSH","CO-SSM","CO-Salience", "CO-VAN", "CO-Visual", "CP-Auditory", "CP-CO", "CP-CP", "CP-DMN", "CP-DAN", 
+"CP-FP", "CP-none", "CP-retrosplenial temporal", "CP-SSH", "CP-SSM", "CP-Salience", "CP-VAN", "CP-Visual", "DMN-Auditory", "DMN-CO", "DMN-CP", "DMN-DMN",
+"DMN-DAN", "DMN-FP", "DMN-none", "DMN-retrosplenial temporal", "DMN-SSH", "DMN-SSM", "DMN-Salience", "DMN-VAN", "DMN-Visual", "DAN-Auditory", "DAN-CO",
+"DAN-CP", "DAN-DMN", "DAN-DAN", "DAN-FP","DAN-none", "DAN-retrosplenial temporal", "DAN-SSH", "DAN-SSM", "DAN-Salience", "DAN-VAN", "DAN-Visual",
+"FP-Auditory", "FP-CO", "FP-CP", "FP-DMN", "FP-DAN", "FP-FP", "FP-none", "FP-retrosplenial temporal", "FP-SSH", "FP-SSM","FP-Salience",
+"FP-VAN", "FP-Visual", "none-Auditory", "none-CO", "none-CP", "none-DMN", "none-DAN", "none-FP", "none-none", "none-retrosplenial temporal", "none-SSH",
+"none-SSM", "none-Salience", "none-VAN", "none-Visual","retrosplenial temporal-Auditory", "retrosplenial temporal-CO", "retrosplenial temporal-CP", 
+"retrosplenial temporal-DMN", "retrosplenial temporal-DAN", "retrosplenial temporal-FP", "retrosplenial temporal-none", "retrosplenial temporal-retrosplenial temporal",
+"retrosplenial temporal-SSH", "retrosplenial temporal-SSM", "retrosplenial temporal-Salience", "retrosplenial temporal-VAN", "retrosplenial temporal-Visual",
+"SSH-Auditory","SSH-CO","SSH-CP","SSH-DMN","SSH-DAN","SSH-FP","SSH-none","SSH-retrosplenial temporal","SSH-SSH","SSH-SSM","SSH-Salience","SSH-VAN",
+"SSH-Visual","SSM-Auditory","SSM-CO","SSM-CP","SSM-DMN","SSM-DAN","SSM-FP","SSM-none","SSM-retrosplenial temporal","SSM-SSH","SSM-SSM","SSM-Salience",
+"SSM-VAN","SSM-Visual","Salience-Auditory","Salience-CO","Salience-CP","Salience-DMN","Salience-DAN","Salience-FP","Salience-none","Salience-retrosplenial temporal",
+"Salience-SSH","Salience-SSM","Salience-Salience","Salience-VAN","Salience-Visual","VAN-Auditory","VAN-CO","VAN-CP","VAN-DMN","VAN-DAN","VAN-FP","VAN-none",
+"VAN-retrosplenial temporal","VAN-SSH","VAN-SSM","VAN-Salience","VAN-VAN","VAN-Visual","Visual-Auditory","Visual-CO","Visual-CP","Visual-DMN","Visual-DAN",
+"Visual-FP","Visual-none","Visual-retrosplenial temporal","Visual-SSH","Visual-SSM","Visual-Salience","Visual-VAN","Visual-Visual"]
---
+++ gordon_correlations_algorithms/create_data_file.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+==========================================================================================
+Split ABCD subjects (after post-processing ) to 3 datasets with the following percentage:
+60% train, 20% validation and 20% test. The ABCD data is augmented to create more volume of data.
+==========================================================================================
+
+@Input:  
+min_scan_len = integer. Number of volumes for subjects exclusion. Default is 375 (5 minutes scan)
+subjects_data_dict = string. path to pkl file containing the subjects data dictionary after post processing
+score_file = string. path to excel file containing the behavioural scores
+score_key = string. name of the test score column
+classification = bool. If flag exists, creates 3 classes instade of regression values
+out_folder = string. path to output folder. Default is current folder. 
+
+@Output:
+train_set.pkl file: train dataset
+validate_set.pkl file: validate dataset 
+test_set.pkl file: test dataset
+Each dataset is of type List of Tuples. Tuple = (time_series, score) 
+
+"""
+
+import os, sys, pickle, argparse, re
+import pandas as pd
+from Gordon_networks import networks_list
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--corr_file', required=True, type=str, help='path to excel file containing Gordon correlations')
+	parser.add_argument('--score_file', required=True, type=str, help='path to excel file containing the behavioural scores')
+	parser.add_argument('--score_key', required=True, type=str, help='name of the test score column')
+	parser.add_argument('--out_folder', required=False, type=str, default=".", help='path to output folder. Default is current folder')
+	args = parser.parse_args()
+
+
+	#Read the excel files 
+	df_corr = pd.read_excel(io=args.corr_file)
+	df_scores = pd.read_excel(io=args.score_file)
+	#Read the Y excel file
+	df_scores = df_scores[['SUBJECTKEY'] + [args.score_key]]
+	data_df = pd.merge(left=df_corr, right=df_scores, left_on='SUBJECTKEY', right_on='SUBJECTKEY').dropna()
+	data_df = data_df.drop('SUBJECTKEY', axis = 1)
+	cognitive_networks = ["CO-CO","CO-DMN","CO-DAN","CO-FP","CO-Salience", "CO-VAN","DMN-DMN","DMN-DAN", "DMN-FP","DMN-Salience", "DMN-VAN",
+	"DAN-DAN", "DAN-FP","DAN-Salience", "DAN-VAN", "FP-FP","FP-Salience","FP-VAN","Salience-Salience","Salience-VAN","VAN-VAN"]
+	indexes = [] 
+	for net in cognitive_networks:
+		indexes.append(networks_list.index(net))
+	indexes.append(-1)
+	data_df.rename(columns={args.score_key:'label'}, inplace=True)
+	data_df = data_df.iloc[:,indexes]
+
+	
+	#Save the data
+	data_df.to_csv(path_or_buf=args.out_folder + "/all_data.csv", index=False)
+	 
+ 
