3500944f674941c512536af5bd4b920e66c4fa5f
Vicki Khalfin <khalfinv@gmail.com>
2020-06-03 11:54:13 +0300

    added f1_macro and f1_weighted

--- cognitive_tests_algorithms/run_cv.py
+++ cognitive_tests_algorithms/run_cv.py
@@ -9,7 +9,7 @@ def run_train_test(cv_data_folder, i):
 	subprocess.call("train.py --data_folder " + cv_path +  " --out_folder " + cv_path , shell=True)
 	validate_set_path = os.path.join(cv_path,"validate_set.pkl")
 	model_path = os.path.join(cv_path,"cogtests_nn_model.pkl")
-	return test.main(validate_set_path,model_path,cv_path)
+	return test.testFunc(validate_set_path,model_path,cv_path)
 
 def collect_errors(exception):
 	""" Callback for errors collecting from threads. Get the exception and write to file
@@ -17,7 +17,7 @@ def collect_errors(exception):
 	"""
 	print(exception)
 	
-def collect_results(result):
+def collect_results(accuracy, f1_macro, f1_weighted):
 	"""Collect the results from postProcessing function. 
 	   Insert the result to allParticipantsDic.
 	param result: dictionary raw. 
@@ -27,7 +27,12 @@ def collect_results(result):
 	return: None 
 	"""
 	global total_acc
-	total_acc+=result
+	global total_f1_macro
+	global total_f1_weighted
+	total_acc+=accuracy
+	total_f1_macro+=f1_macro
+	total_f1_weighted+=f1_weighted
+	
 	
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
@@ -35,6 +40,8 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	K = 10
 	total_acc = 0
+	total_f1_macro = 0
+	total_f1_weighted = 0
 	pool = mp.Pool()
 	for i in range(K):
 		[pool.apply_async(run_train_test, args=(args.cv_data_folder, i,),callback=collect_results, error_callback = collect_errors)]
@@ -43,4 +50,6 @@ if __name__ == "__main__":
 
 		
 	print("Average accuracy:", total_acc/K)
+	print("Average f1 macro:", total_f1_macro/K)
+	print("Average f1 weighted:", total_f1_weighted/K)
 
