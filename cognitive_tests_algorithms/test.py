29500bd324c882f295dcc70e119ed8e086ded6bb
Vicki Khalfin <khalfinv@gmail.com>
2020-06-03 11:56:28 +0300

    added confusion matrix and classification report

--- cognitive_tests_algorithms/test.py
+++ cognitive_tests_algorithms/test.py
@@ -11,10 +11,12 @@ import torch
 import argparse
 import common
 import os
+import sklearn.metrics as skm
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
 
 input_size = 52
-accuracy = 0
-num_classes = 2
 		   
 def load_checkpoint(model,filepath):
 	"""Load the model
@@ -28,66 +30,68 @@ def load_checkpoint(model,filepath):
 	model.load_state_dict(state['state_dict'])
 	return model
 
-	
-def testFunc(net, test_loader, out_folder): 
-	"""Test the network
-	param net: nn.Module. The network to test
-	param test_loader: data.Dataset. The test dataset
-
-	return: accuracy rate
+def plot_confusion_matrix(cm, classes, out_file,
+						  title='Confusion matrix',
+						  cmap=plt.cm.Blues):
 	"""
-	total = 0 # sum of total scores 
-	correct = 0
-	net.eval() # turning the network to evaluation mode, affect dropout and batch-norm layers if exists
-	num_of_below_avg = 0
-	num_of_avg = 0
-	num_of_above_avg = 0
-	nb_classes = num_classes
-	conf_matrix = torch.zeros(nb_classes, nb_classes)
-	for i, (correlations, scores) in enumerate(test_loader):
-		correlations = common.to_cuda(correlations)
-		outputs = net(correlations)
-		_, predicted = torch.max(outputs, 1)
-		if(predicted == 0):
-			num_of_below_avg+=1
-		elif(predicted == 1):
-			num_of_above_avg+=1
-		else:
-			num_of_avg+=1
-		total += scores.size(0)
-		correct += (predicted.cpu() == scores).sum()
-		for t, p in zip(scores, predicted.cpu()):
-			conf_matrix[t, p] += 1
-	TP = conf_matrix.diag()
-	for c in range(nb_classes):
-		idx = torch.ones(nb_classes).byte()
-		idx[c] = 0
-		# all non-class samples classified as class
-		FP = float(conf_matrix[idx, c].sum())
-		# all class samples not classified as class
-		FN = float(conf_matrix[c, idx].sum())
-	accuracy = (100*float(correct)/total)
-	out_file = open(os.path.join(out_folder,"results.txt"),'w')
-	out_file.write(str(conf_matrix.numpy()) + "\n")
-	out_file.write("False Positive, 0->1:" + str(FP) + " " + "%.3f" % ((FP/(conf_matrix[0,0]+conf_matrix[0,1]))*100) + "%" + " False Negative, 1->0:" + str(FN) + " " + "%.3f" % ((FN/(conf_matrix[1,0]+conf_matrix[1,1]))*100) + "%" + "\n")
-	out_file.write("accuracy: " + "%.3f" % accuracy + "\n")
-	out_file.close()
-	return accuracy
+	This function prints and plots the confusion matrix.
+	Normalization can be applied by setting `normalize=True`.
+	"""
+	plt.figure()
+	plt.imshow(cm, interpolation='nearest', cmap=cmap)
+	plt.title(title)
+	plt.colorbar()
+	tick_marks = np.arange(len(classes))
+	plt.xticks(tick_marks, classes, rotation=45)
+	plt.yticks(tick_marks, classes)
+
+	thresh = cm.max() / 2.
+	for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+		plt.text(j, i, cm[i, j],
+				 horizontalalignment="center",
+				 color="white" if cm[i, j] > thresh else "black")
+
+	plt.tight_layout()
+	plt.ylabel('True label')
+	plt.xlabel('Predicted label')
+	plt.savefig(out_file)
 	
-def main(test_dataset_path, model_path, out_folder ):
-	
+def testFunc(test_dataset_path, model_path, out_folder ):
+	#Load the test data
 	test_dataset = common.CognitiveTestsDataset(test_dataset_path)
-	
-	test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-											   shuffle=False)
+	#Open the output file						
+	out_file = open(os.path.join(out_folder,"results.txt"),'w')
+	#Explore the classes
+	values, counts = np.unique(test_dataset.y, return_counts=True)
+	num_classes = len(values)
+	percentage = counts / len(test_dataset.X)
+	out_file.write("The classes: " + str(values) + " The frequency: " + str(counts) + " The percentage: " + str(percentage) + "\n")
+	#Initiate the network
+	input_size = len(test_dataset.X[0])
 	net = common.cogtests_nn(input_size, num_classes)
 	#load the model						  
 	net = load_checkpoint(net, model_path)
 	net = common.to_cuda(net)
 	# Test the Model
-	accuracy = testFunc(net, test_loader, out_folder)
-	print ("%.3f" % accuracy)
-	return accuracy
+	net.eval() # turning the network to evaluation mode, affect dropout and batch-norm layers if exists
+	correlations = torch.FloatTensor(test_dataset.X)
+	correlations = common.to_cuda(correlations)
+	outputs = net(correlations)
+	_, predicted = torch.max(outputs, 1)
+	#Output the results metrics
+	confusion_matrix = skm.confusion_matrix(test_dataset.y, predicted)
+	plot_confusion_matrix(confusion_matrix,values,os.path.join(out_folder,'confusion matrix.png'))
+	out_file.write("Confusion matrix: \n" + str(confusion_matrix) + "\n\n")
+	accuracy = skm.accuracy_score(test_dataset.y, predicted)
+	out_file.write(skm.classification_report(test_dataset.y, predicted))
+	f1_score_macro = skm.f1_score(test_dataset.y, predicted, average = 'macro')
+	f1_score_weighted = skm.f1_score(test_dataset.y, predicted, average = 'weighted')
+	print ("accuracy: %.3f " % accuracy)
+	print ("f1_score_macro: %.3f " % f1_score_macro)
+	print ("f1_score_weighted: %.3f  " % f1_score_weighted)
+	return accuracy, f1_score_macro, f1_score_weighted
+	
+	
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--test_dataset', required=True, help='path to test dataset')
@@ -95,4 +99,4 @@ if __name__ == "__main__":
 	parser.add_argument('--out_folder', required=True, help='path to the out folder')
 	args = parser.parse_args()
 	
-	main(args.test_dataset, args.model, args.out_folder)
+	testFunc(args.test_dataset, args.model, args.out_folder)
