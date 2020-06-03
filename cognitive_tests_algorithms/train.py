03fdacbe0356c101bc2100670d4c9c6c3284d538
Vicki Khalfin <khalfinv@gmail.com>
2020-06-03 11:58:42 +0300

    added class exploration and automaticly change of num_of_classes and input_size

--- cognitive_tests_algorithms/train.py
+++ cognitive_tests_algorithms/train.py
@@ -15,13 +15,12 @@ import pandas as pd
 import common
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Hyper Parameters
 num_epochs = 150
 batch_size = 30
 learning_rate = 0.00001
-input_size = 52
-num_classes = 2
 
 	
 def save_checkpoint(model,filepath):
@@ -119,57 +118,69 @@ def evaluateFunc(net, validate_loader, criterion):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data_folder', required=True, help='path to folder contaning all the data - train, validate and test')
-    parser.add_argument('--out_folder', required=True, help='path to folder where to save the model')
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--data_folder', required=True, help='path to folder contaning all the data - train, validate and test')
+	parser.add_argument('--out_folder', required=True, help='path to folder where to save the model')
+	args = parser.parse_args()
 
 
-    train_dataset = common.CognitiveTestsDataset(os.path.join(args.data_folder,"train_set.pkl"))
-        
-    validate_dataset = common.CognitiveTestsDataset(os.path.join(args.data_folder,"validate_set.pkl"))
+	train_dataset = common.CognitiveTestsDataset(os.path.join(args.data_folder,"train_set.pkl"))
+		
+	validate_dataset = common.CognitiveTestsDataset(os.path.join(args.data_folder,"validate_set.pkl"))
 
 
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                               batch_size=batch_size,
-                                               shuffle=True)
-                                              
-    validate_loader = torch.utils.data.DataLoader(dataset=validate_dataset,
-                                           batch_size=batch_size,
-                                           shuffle=False)
+	train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+											   batch_size=batch_size,
+											   shuffle=True)
+											  
+	validate_loader = torch.utils.data.DataLoader(dataset=validate_dataset,
+										   batch_size=batch_size,
+										   shuffle=False)
 										   
-											   
-    #create object of the cogtests_nn class
-    cogtests_nn = common.cogtests_nn(input_size, num_classes)
-    cogtests_nn = common.to_cuda(cogtests_nn)
-    # Loss and Optimizer
-    #criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.34, 0, 0.66]))
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(cogtests_nn.parameters(),lr = learning_rate)
-    trainLossArr = []
-    trainErrArr = []
-    evaluateLossArr = []
-    evaluateErrArr = []
-    #Iterate num_epoch times and in each epoch train on total data amount / batch_size
-    for epoch in range(num_epochs):
-        trainLoss, trainErr = trainFunc(cogtests_nn, train_loader, criterion, optimizer)
-        evaluateLoss, evaluateErr  = evaluateFunc(cogtests_nn, validate_loader, criterion)
-        
-        trainLossArr.append(trainLoss)
-        trainErrArr.append(trainErr)
-        evaluateLossArr.append(evaluateLoss)
-        evaluateErrArr.append(evaluateErr)
+	#Explore the classes
+	values, counts = np.unique(train_dataset.y, return_counts=True)
+	num_classes = len(values)
+	print("The classes: " + str(values))
+	percentage = counts / len(train_dataset.X)
+	print("Train: The frequency: " + str(counts) + " The percentage: " + str(percentage) + "\n")
 
-    #Save the Model
-    save_checkpoint(cogtests_nn, os.path.join(args.out_folder, "cogtests_nn_model.pkl"))
+	values, counts = np.unique(validate_dataset.y, return_counts=True)
+	percentage = counts / len(validate_dataset.X)
+	print("Validate: The frequency: " + str(counts) + " The percentage: " + str(percentage) + "\n")
 
-    #plot graphs
-    #err vs epoch
-    plotGraph('Error vs Epoch', range(num_epochs), trainErrArr, evaluateErrArr, 'Epoch', 'Error', os.path.join(args.out_folder, "errPlot.png"))
-    #loss vs epoch
-    trainLossArr = [round(loss,3) for loss in trainLossArr]
-    testLossArr = [round(loss,3) for loss in evaluateLossArr]
-    plotGraph('Loss vs Epoch', range(num_epochs), trainLossArr, evaluateLossArr, 'Epoch', 'Loss', os.path.join(args.out_folder, "lossPlot.png"))
+	#Initiate the network
+	input_size = len(train_dataset.X[0])									   
+	#create object of the cogtests_nn class
+	cogtests_nn = common.cogtests_nn(input_size, num_classes)
+	cogtests_nn = common.to_cuda(cogtests_nn)
+	# Loss and Optimizer
+	#criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.34, 0, 0.66]))
+	criterion = nn.CrossEntropyLoss()
+	optimizer = torch.optim.Adam(cogtests_nn.parameters(),lr = learning_rate)
+	trainLossArr = []
+	trainErrArr = []
+	evaluateLossArr = []
+	evaluateErrArr = []
+	#Iterate num_epoch times and in each epoch train on total data amount / batch_size
+	for epoch in range(num_epochs):
+		trainLoss, trainErr = trainFunc(cogtests_nn, train_loader, criterion, optimizer)
+		evaluateLoss, evaluateErr  = evaluateFunc(cogtests_nn, validate_loader, criterion)
+		
+		trainLossArr.append(trainLoss)
+		trainErrArr.append(trainErr)
+		evaluateLossArr.append(evaluateLoss)
+		evaluateErrArr.append(evaluateErr)
+
+	#Save the Model
+	save_checkpoint(cogtests_nn, os.path.join(args.out_folder, "cogtests_nn_model.pkl"))
+
+	#plot graphs
+	#err vs epoch
+	plotGraph('Error vs Epoch', range(num_epochs), trainErrArr, evaluateErrArr, 'Epoch', 'Error', os.path.join(args.out_folder, "errPlot.png"))
+	#loss vs epoch
+	trainLossArr = [round(loss,3) for loss in trainLossArr]
+	testLossArr = [round(loss,3) for loss in evaluateLossArr]
+	plotGraph('Loss vs Epoch', range(num_epochs), trainLossArr, evaluateLossArr, 'Epoch', 'Loss', os.path.join(args.out_folder, "lossPlot.png"))
 
 
 	
