414a6ece4cbee32bb3c369dbc515137c386cd42e
Vicki Khalfin <khalfinv@gmail.com>
2020-12-19 23:12:31 +0200

    atlas test modified

--- networks_correlations_code/Atlases/testDic.py
+++ networks_correlations_code/Atlases/testDic.py
@@ -1,15 +1,25 @@
 from networkToIndexDicGordon import dic
+import pandas as pd
+import argparse
+from nilearn import plotting
+import numpy as np
 
-total = 0
-all_indexes = []
-for key,val in dic.items():
-	print(key)
-	print(len(val))
-	all_indexes = all_indexes + val
-	total += len(val)
-print ("total = ", total)
-print(all_indexes)
-all_indexes_set = set(all_indexes)
-contains_duplicates = len(all_indexes) != len(all_indexes_set)
-
-print(contains_duplicates)
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--dic_excel', required=True, type=str, help='path to dictionary excel file')
+	args = parser.parse_args()
+	fd = pd.read_excel(args.dic_excel)
+	for net, indexes in dic.items():
+		fd_net	= fd[fd["Community"] == net]
+		parcels = list(fd_net["ParcelID"])
+		parcels = list(np.array(parcels) - np.array([1]*len(parcels)))
+		if (indexes != parcels):
+			print("Parcelation error in " , net)
+		coords = fd_net["Centroid (MNI)"]
+		coords_new = []
+		for coord_str in coords:
+			coords_new.append([float (x) for x in coord_str.split()])
+		view = plotting.view_markers(coords_new, marker_size=10) 
+		html_name = "test_out/network_plot" + str(net) + ".html"
+		view.save_as_html(html_name)
+		
