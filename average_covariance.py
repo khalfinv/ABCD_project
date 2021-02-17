b8821d9ab03b5910fc6a12695504760c929309a0
vicki <khalfinv@gmail.com>
2021-02-17 15:00:11 +0200

    script for creating mean covariance and correlation matrices

---
+++ average_covariance.py
@@ -0,0 +1,36 @@
+from pyriemann import utils
+import argparse
+import pickle
+import numpy as np
+import pandas as pd
+import nilearn
+from nilearn.connectome import cov_to_corr 
+
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--out_folder', required=True, type=str, help='output folder')
+	parser.add_argument('--subjects_dic', required=True, type=str, help='path to pkl file with all the subjects')
+	args = parser.parse_args()
+	
+	pkl_file = open(args.subjects_dic, 'rb')
+	allParticipantsDict = pickle.load(pkl_file)
+	pkl_file.close()
+	
+	num_of_subjects = len(allParticipantsDict)
+	print(num_of_subjects)
+	
+	all_mat = []
+	
+	for val in allParticipantsDict.values():
+		all_mat.append(val["covariance"])
+	
+	print(np.array(all_mat).shape)
+	avg_cov_mat = utils.mean.mean_covariance(np.array(all_mat))
+	avg_cor_mat = nilearn.connectome.cov_to_corr(avg_cov_mat) 
+	
+	df = pd.DataFrame(avg_cor_mat)
+	df.to_excel(args.out_folder + "/avg_correlation_matrix.xlsx")
+	df = pd.DataFrame(avg_cov_mat)
+	df.to_excel(args.out_folder + "/avg_covariance_matrix.xlsx")
